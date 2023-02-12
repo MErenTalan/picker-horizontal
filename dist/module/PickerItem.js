640fc91dd14d8a5701eb02791202bf819fac9690
MErenTalan <110690535+MErenTalan@users.noreply.github.com>
2023-02-12 14:34:04 +0300

    a

---
+++ dist/module/PickerItem.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) Nicolas Gallagher.
+ *
+ *
+ *
+ */
+import * as React from "react";
+import * as ReactNativeWeb from "react-native-web";
+
+const Option = (props) =>
+  ReactNativeWeb.unstable_createElement("option", props);
+
+const CustomText = (props) =>
+  ReactNativeWeb.unstable_createElement("span", props);
+/**
+ * PickerItem Component for React Native Web
+ * @returns
+ */
+
+export default function PickerItem({
+  color,
+  label,
+  testID,
+  value,
+  enabled = true,
+}) {
+  return /*#__PURE__*/ React.createElement(
+    Option,
+    {
+      disabled: enabled === false ? true : undefined,
+      style: {
+        color,
+      },
+      testID: testID,
+      value: value,
+      label: label,
+    },
+    /*#__PURE__*/ React.createElement(
+      CustomText,
+      {
+        style: {
+          backgroundColor: "#ff0000",
+        },
+      },
+      label
+    )
+  );
+}
+//# sourceMappingURL=PickerItem.js.map
---
+++ dist/module/PickerMacOS.web.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import UnimplementedView from "./UnimplementedView";
+
+function PickerMacOS() {
+  return /*#__PURE__*/ React.createElement(UnimplementedView, null);
+}
+/**
+ * Fallback for other platforms
+ */
+
+PickerMacOS.Item = UnimplementedView;
+export default PickerMacOS;
+//# sourceMappingURL=PickerMacOS.web.js.map
