c7432250bed3630b481285845e74c497a6cc3c9c
MErenTalan <110690535+MErenTalan@users.noreply.github.com>
2023-02-12 14:22:01 +0300

    a

---
+++ js/PickerItem.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) Nicolas Gallagher.
+ *
+ * @flow
+ *
+ */
+
+import type {ColorValue} from 'react-native/Libraries/StyleSheet/StyleSheet';
+
+import * as React from 'react';
+import * as ReactNativeWeb from 'react-native-web';
+
+type Props = {
+  color?: ColorValue,
+  label: string,
+  testID?: string,
+  enabled?: boolean,
+  value?: number | string,
+};
+
+const Option = (props: any) =>
+  ReactNativeWeb.unstable_createElement('option', props);
+
+const CustomText = (props: any) =>
+ReactNativeWeb.unstable_createElement('span', props);
+
+/**
+ * PickerItem Component for React Native Web
+ * @returns
+ */
+export default function PickerItem({
+  color,
+  label,
+  testID,
+  value,
+  enabled = true,
+}: Props): React.Node {
+  return (
+    <Option
+      disabled={enabled === false ? true : undefined}
+      style={{color}}
+      testID={testID}
+      value={value}
+      label={label}>
+     <CustomText style={{backgroundColor:"#ff0000"}}>{label}</CustomText>
+    </Option>
+  );
+}
